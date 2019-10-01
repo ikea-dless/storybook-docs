84dddeb7f6a306c40cd8fec3af065ca5e65333a0
Takahiro Ikeda <takahiro.ikeda@livesense.co.jp>
2019-10-01 16:26:59 +0900

    init

---
+++ stories/1-Button.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+
+export default {
+  title: 'Button',
+  component: text,
+  parameters: {
+    componentSubtitle: 'サブタイトルが入ります'
+  }
+};
+
+export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+
+export const emoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+      😀 😎 👍 💯
+    </span>
+  </Button>
+);
